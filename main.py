a9ae849a6d9a56f4db99e3ff703dd1766b160ed0
Owen b <31399167+NULLtm@users.noreply.github.com>
2023-08-06 18:39:01 -0700

    Update main.py
    getting there -- more to be done

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from datetime import datetime
 import pause
 import smtplib
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import time
 
 load_dotenv()
@@ -35,6 +38,9 @@ DAY = os.environ.get("DAY")
 TIME = os.environ.get("TIME")
 AM_PM = os.environ.get("AM_PM")
 
+RESTAURANT = os.environ.get("RESTAURANT")
+PARTY_SIZE = os.environ.get("PARTY_SIZE")
+
 
 def send_message():
     recipient = PHONE_NUMBER + CARRIERS[CARRIER]
@@ -47,18 +53,32 @@ def send_message():
 
 driver = webdriver.Chrome()
 driver.get("https://www.exploretock.com/tfl/")
-time.sleep(5)
+WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//span[text()='Log in']//..")))
 driver.find_element(By.XPATH, "//span[text()='Log in']//..").click()
-time.sleep(5)
+WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "email")))
+WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "password")))
+WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//button[@data-testid='signin']")))
 driver.find_element(By.ID, "email").send_keys(USERNAME)
 driver.find_element(By.ID, "password").send_keys(PASSWORD)
 driver.find_element(By.XPATH, "//button[@data-testid='signin']").click()
-time.sleep(5)
-driver.find_element(By.XPATH, "//a[@data-testid='offering-book-button_DiningRoom']").click()
+WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//button[@data-testid='search-button']")))
+driver.find_element(By.XPATH, "//button[@data-testid='search-button']").click()
+WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//input[@aria-label='Search']")))
+driver.find_element(By.XPATH, "//input[@aria-label='Search']").send_keys(RESTAURANT)
+WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class,'SearchBar-results')]//ul//li")))
+time.sleep(0.5)
+driver.find_elements(By.XPATH, "//div[contains(@class,'SearchBar-results')]//ul//li")[2].click()
 
+print("On restaurant home page, now waiting for release time...")
 dt = datetime(int(RELEASE_YEAR), int(RELEASE_MONTH), int(RELEASE_DAY), int(RELEASE_TIME_24_HOUR_UTC))
 pause.until(dt)
 print("GOT TO TIME")
+
+people = driver.find_element(By.XPATH, "//div[@class='GuestSelector-text']")
+driver.execute_script(f"arguments[0].selected-value = {PARTY_SIZE};", people)
+
+
+
 month = driver.find_element(By.XPATH, f"//span[text()='{MONTH + ' ' + YEAR}']//..//..//..")
 month.find_element(By.XPATH, f"//span[text()={DAY}]//..").click()
 time.sleep(3)
